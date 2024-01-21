c628199ab344be18a24e99897c6d56de6ae23fcc
Nguyễn Tiến Anh Huy <nhingiday219@gmail.com>
2024-01-22 03:18:28 +0700

    BaiTapNgay29/1

---
+++ MaTran.js
@@ -0,0 +1,8 @@
+//Bai3: MaTrix ngay 19/1
+var a=[[1,1,1],[1,0,0],[0,1,0],[0,0,1]];
+for (var i=0;i<4;i++){
+    for( var j=0 ; j<3;j++){
+        document.write(a[i][j] + " " );
+    }
+     document.write("<br/>")
+}
