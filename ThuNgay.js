221ff27a4cc552d5b13faa8d990a86fff7f95ddb
Nguyễn Tiến Anh Huy <nhingiday219@gmail.com>
2024-01-22 03:00:39 +0700

    BaiTapLapTrinhJS

---
+++ ThuNgay.js
@@ -0,0 +1,27 @@
+//BaiTap1-Ngay19/1/2024
+var y =["Thứ hai" , "Thứ ba" ,"Thứ tư","Thứ năm" ,"Thứ sáu","Thứ bảy","Chủ nhật"];
+ var x = 5;
+ console.log(y[x-2]);
+ switch(x){
+    case 2:
+        console.log("Thứ hai");
+        break;
+    case 3:
+        console.log("Thứ ba");
+        break;
+    case 4:
+        console.log("Thứ tư");
+        break;
+    case 5:
+        console.log("Thứ năm");
+        break;
+    case 6:
+        console.log("Thứ sáu");
+        break;
+    case 7:
+        console.log("Thứ bảy");
+        break;
+    case 8:
+        console.log("Chủ nhật");
+        break;
+ }
